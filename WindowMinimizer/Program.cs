f6d8cd851bcd19e1ae01d89fcebd20f5b9927fda
nightsuperthunder <slavazabolotiv@gmail.com>
2021-08-15 20:46:03 +0300

    init commit

---
+++ WindowMinimizer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace WindowMinimizer {
+    internal static class Program {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [STAThread]
+        private static void Main()
+        {
+            Process[] processesByName = Process.GetProcessesByName(Environment.GetCommandLineArgs()[0]);
+            if (processesByName.Length == 0)
+                return;
+            foreach (Process process in processesByName)
+                ShowWindow(process.MainWindowHandle, 6);
+        }
+    }
+}
